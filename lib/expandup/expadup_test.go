3212b9c840a3dd68e8d936c964fc5ec268562b7c
Qs-F <qsf@de-liker.com>
2020-01-27 03:48:26 +0900

    Add test for Comp()

--- lib/expandup/expadup_test.go
+++ lib/expandup/expadup_test.go
@@ -5,3 +5,15 @@ import "testing"
 // single test for constructor
 func TestNewDef(t *testing.T) {
 }
+
+func TestComp(t *testing.T) {
+	target1 := "hello world"
+	target2 := "hello world"
+
+	sum1 := Sum(target1)
+	sum2 := Sum(target2)
+
+	if !Comp(sum1, sum2) {
+		t.Error("Comp failed")
+	}
+}
