970808d62d794ad9a9cad300fec40f811e6bf45d
Qs-F <qsf@de-liker.com>
2020-01-29 16:08:53 +0900

    Add fext test

--- lib/expandup/resolve_test.go
+++ lib/expandup/resolve_test.go
@@ -1,1 +1,36 @@
 package expandup
+
+import (
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func TestFextSingle(t *testing.T) {
+	basepath := "./_testdata/fext/"
+
+	type T struct {
+		Input path
+		Must  *files
+	}
+
+	tests := []T{
+		{
+			Input: NewPath(filepath.Join(basepath, "./testfile")),
+			Must: &files{
+				&file{
+					Path:    NewPath(filepath.Join(basepath, "./testfile")),
+					Content: []byte{},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		if result := fext(test.Input); !reflect.DeepEqual(result, test.Must) {
+			t.Errorf("expect %v but got %v\n", test.Must, result)
+		} else {
+			t.Logf("got %v\n", result)
+		}
+	}
+}
